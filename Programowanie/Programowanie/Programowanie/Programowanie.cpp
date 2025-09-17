cfa891aabec92c520577b09fb8092e7010b5e0c8
Wiktor64 <wiktor44412@gmail.com>
2025-09-17 19:35:30 +0100

    Update Programowanie.cpp

--- Programowanie/Programowanie/Programowanie/Programowanie.cpp
+++ Programowanie/Programowanie/Programowanie/Programowanie.cpp
@@ -6,6 +6,8 @@
 int main()
 {
     std::cout << "Hello World!\n";
+    std::cout << "Hello \nWorld!\n";
+    std::cout << 'h';
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
@@ -18,3 +20,4 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
