61e85125ab802ea20842041e49a3ca73da43ba6c
Sala213 <Sala213@ezse.pl>
2025-10-07 09:54:10 +0200

    Koniec lekcji

--- Programowanie_zajecia/VariableConsoleApplication/VariableConsoleApplication.cpp
+++ Programowanie_zajecia/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -75,43 +75,121 @@ void task5()
 	v = (3.14 / 3) * (r * r) * h;
 	std::cout << "Objetosc stozka: " << v << "\n";
 }
-int main()
+//* .Program obliczający pole trójkąta o podstawie b i wysokości h
+void task6()
 {
-	task5();
+	int a;
+	std::cout << "Podaj liczbe a \n";
+	std::cin >> a;
+	int b;
+	std::cout << "Podaj liczbe b \n";
+	std::cin >> b;
+	int wynik;
+	wynik = a * a + b * b;
+	std::cout << "Wynik dzialania a^a + b^b wynosi :" << wynik;
+}
+//. Program obliczający pole trójkąta o podstawie b i wysokości h
+void task7()
+{
+	int a;
+	std::cout << "Podaj podstawe trójkąta (a): \n";
+	std::cin >> a;
+	int h;
+	std::cout << "Podaj wysokość (h)";
+	std::cin >> h;
+	int wynik;
+	wynik = a * h / 2;
+	std::cout << "Wynik pola trójkąta (a*h\2) :" << wynik;
+}
+
+////* Program obliczający objętość kuli o promieniu r
+void task8()
+{
+	float r;
+	std::cout << "Podaj liczbe (r): \n";
+	std::cin >> r;
+	float v;
+
+	v = 4.0 / 3.0 * 3.14 * (r * r * r);
+	std::cout << "Wynik objętości koła wynosi:" << v;
+}
+///* .Program obliczający pole trapezu o podstawach a i b oraz wysokości h
+void task9()
+{
+	int a;
+	std::cout << "Podaj podstawe trapezu (a):";
+	std::cin >> a;
+	int b;
+	std::cout << "Podaj pole drugiej podstawy (b):";
+	std::cin >> b;
+	int h;
+	std::cout << "Podaj wysokość (h)";
+	std::cin >> h;
+	int w;
+	w = ((a + b) * h) / 2;
+	std::cout << "Wynik pola trapezu wynosi:" << w;
+}
+//* .Program obliczający średnią ważoną trzech liczb z podanymi odpowiednimi wagami w1, w2 i w3.
+void task10()
+{
+	float l1;
+	std::cout << "Podaj Liczbe (1):";
+	std::cin >> l1;
+	float l2;
+	std::cout << "Podaj Liczbe (2):";
+	std::cin >> l2;
+	float l3;
+	std::cout << "Podaj Liczbe (3):";
+	std::cin >> l3;
+	float w1;
+	std::cout << "Podaj wage do liczby (1):";
+	std::cin >> w1;
+	float w2;
+	std::cout << "Podaj wage do liczby (2):";
+	std::cin >> w2;
+	float w3;
+	std::cout << "Podaj wage do liczby (3):";
+	std::cin >> w3;
+	float w, sumaWag;
+
+	sumaWag = (w1 + w2 + w3);
+
+	w = (l1 * w1 + l2 * w2 + l3 * w3) / sumaWag;
+	std::cout << "Wynik średniej ważonej to :" << w;
+}
+void task11()
+{
+	int m, cm, mm;
+	std::cout << "Podaj ilość metrów ,które mają być przekształcone na cm lub milimetry";
+	std::cin >> m , mm;
+	cm = m * 100;
+	mm = m * 1000;
+
+	std::cout << "Twoje metry w centymetrach   :" << cm << "\nTwoje metry w milimetrach   :" << mm;
 }
 
 
-/*
+void task12()
+{
+	float pln, usd;
+	std::cout << "Podaj kwote PLN ,która ma być przeliczona na USD";
+	std::cin >> pln;
+	
+	usd = pln * 3.65;
+	std::cout << "Twoja suma z PLN na USD wynosi :" << usd;
 
-Zmienna - pewien obszar w pamięci operacyjnej, w której można
-w danej chwili przechować tylko jedną daną.
 
-Instrukcja daklaracji zmiennej:
-typ_zmienej nazwaZmiennej;
 
-Typ zmiennej - wielkość obszaru pamięci, interpretacja ciągu bitów
 
-int - 4 bajtowa liczba całkowita ze znakiem <-2 147 483 648, 2 147 483 647>
 
-Nazwa zmiennej - nazwa obszaru w pamięci, identyfikator
-Warunki niezbędne:
-* dozwolone znaki:
-	- alfabet angielski aA-zZ
-	- cyfry arabskie 0-9
-	- podkreślenie (podłoga) _
-* pierwszym znakiem nie może być cyfra
-* unikalny w swoim zakresie widoczności
-* nie może to być słowo kluczowe (zarezerwowane) danego języka
 
-Warunki programistów:
-* nazwa zmiennej powinna oddawać charakter przechowywanych danych
-* jeśli wiele słów to w miejscu spacji podkreślenie lub zaczynając od drugiego
-  słowa piszemy je z dużej litery
-* piszemy po angielsku
-*/
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+}
+
+int main()
+{
+	setlocale(LC_CTYPE, "polish");
+	task12();
+}
+
+//* .Napisz program, który konwertuje wprowadzoną kwotę w jednej walucie na inną, korzystając z określonego kursu wymiany.
+// 3,65
